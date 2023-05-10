19bf179eb115c77bd32deed58003c9fec769d7c5
pwitowski <pwitowski@omecon.pl>
2023-05-10 12:28:10 +0200

    Feat: add beer controller

---
+++ beer-service/src/main/java/pl/tim3erland/beerservice/web/contoller/BeerContoller.java
@@ -0,0 +1,28 @@
+package pl.tim3erland.beerservice.web.contoller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.tim3erland.beerservice.web.model.BeerDto;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerContoller {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerID) {
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> saveNewBeer(@RequestBody BeerDto beerDto) {
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<Void> updateBeerById(@PathVariable("beerId") UUID beerID, @RequestBody BeerDto beerDto) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
