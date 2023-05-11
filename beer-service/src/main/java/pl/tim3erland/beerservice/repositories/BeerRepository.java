f56b013948e8c9c5b643d37d888829a532c1c289
pwitowski <pwitowski@omecon.pl>
2023-05-11 12:04:49 +0200

    Feat: add beer dto validation

--- beer-service/src/main/java/pl/tim3erland/beerservice/repositories/BeerRepository.java
+++ beer-service/src/main/java/pl/tim3erland/beerservice/repositories/BeerRepository.java
@@ -1,9 +1,9 @@
 package pl.tim3erland.beerservice.repositories;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import pl.tim3erland.beerservice.domain.Beer;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends CrudRepository<Beer, UUID> {
 }
